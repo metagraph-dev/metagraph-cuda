0ae35489dd1c8ade4b4de0503f9764b4757afdcd
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-03-25 14:27:48 -0700

    Initial commit of two tests for triangle count for cugraph.DiGraph
    This commit includes 2 tests for triangle count.
    
    test_triangle_count_on_cugraph_digraph_trivial tests that triangle count works for the trivial case of a fully connected in both directions 3-node graph.
    
    test_triangle_count_on_cugraph_digraph_fully_connected_Graph tests that triangle count works for a fully connected (in both directions) graph (size set to be 30).

---
+++ metagraph_cuda/tests/test_triangle_count.py
@@ -0,0 +1,54 @@
+import metagraph as mg
+import cugraph
+import cudf
+import io
+
+
+def test_triangle_count_on_cugraph_digraph_trivial():
+    # Load Graph Data
+    data = """
+0,1
+1,0
+
+1,2
+2,1
+
+2,0
+0,2
+"""
+    csv_file = io.StringIO(data)
+    gdf = cudf.read_csv(
+        csv_file, names=["Source", "Destination"], dtype=["int32", "int32"]
+    )
+    g = cugraph.DiGraph()
+    g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
+    # Validate Graph Data
+    assert g.number_of_vertices() == 3
+    assert g.number_of_edges() == 6
+    # Test Triangle Count
+    r = mg.resolver
+    assert r.algo.cluster.triangle_count(g) == 1
+
+
+def test_triangle_count_on_cugraph_digraph_fully_connected_Graph():
+    # Generate & Load Graph Data
+    number_of_nodes = 30
+    sources = []
+    destinations = []
+    for node_a in range(number_of_nodes):
+        for node_b in range(node_a):
+            sources.append(node_a)
+            destinations.append(node_b)
+            sources.append(node_b)
+            destinations.append(node_a)
+    g = cugraph.DiGraph()
+    gdf = cudf.DataFrame({"Source": sources, "Destination": destinations})
+    g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
+    # Validate Graph Data
+    assert g.number_of_vertices() == number_of_nodes
+    assert g.number_of_edges() == number_of_nodes * (
+        number_of_nodes - 1
+    )  # n * (n-1) / 2 * two_edges_per_direction
+    # Test Triangle Count
+    r = mg.resolver
+    assert r.algo.cluster.triangle_count(g) == 4060  # 4060 == 30 choose 3
