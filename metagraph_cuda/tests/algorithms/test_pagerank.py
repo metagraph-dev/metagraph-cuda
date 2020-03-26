42f6e81fb40cf356a48cab9d60b7ead6b8a6b4d3
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-03-26 16:35:55 -0700

    Update PageRank tests
    This commit removes all of the old PageRank tests, i.e.
    * test_pagerank_on_cugraph_digraph_via_tiny_fully_connected_graph
    * test_pagerank_on_cugraph_digraph_via_fully_connected_graphs
    * test_pagerank_on_cugraph_digraph_via_star_like_graphs
    
    Those tests tested the backend implementations of PageRank rather than metagraph-cuda itself. Testing the implementation of PageRank is out of scope since the implementation is managed via efforts outside of metagraph-cuda.
    
    We instead added the test test_pagerank_on_cugraph_digraph that tests functionality that is within the scope of metagraph-cuda.
    
    This test does the following:
    * Verify that PageRank is callable.
    * Verify that cugraph.DiGraph is supported directly in the resolver.
    * Verify that the PageRank algorithm is present.
    * Verify that PageRank returns the desired output type (i.e. numpy.ndarray).

--- metagraph_cuda/tests/algorithms/test_pagerank.py
+++ metagraph_cuda/tests/algorithms/test_pagerank.py
@@ -1,91 +1,61 @@
 import metagraph as mg
+import networkx as nx
+import numpy as np
 import cugraph
 import cudf
 import io
-import numpy as np
 
 
-def test_pagerank_on_cugraph_digraph_via_tiny_fully_connected_graph():
-    r = mg.resolver
-    # Load Graph Data
-    data = """
-0,1
-1,0
+def test_pagerank_on_cugraph_digraph():
+    """
+          +-+
+ ------>  |1|
+ |        +-+
+ | 
+ |         |
+ |         v
 
-1,2
-2,1
-
-2,0
-0,2
++-+  <--  +-+       +-+
+|0|       |2|  <--  |3|
++-+  -->  +-+       +-+
 """
-    csv_file = io.StringIO(data)
+    r = mg.resolver
+    # Get Expected networkx Results
+    networkx_graph_data = [
+        (0, 1),
+        (0, 2),
+        (2, 0),
+        (1, 2),
+        (3, 2),
+    ]
+    networkx_graph = nx.DiGraph()
+    networkx_graph.add_edges_from(networkx_graph_data)
+    networkx_pagerank = nx.pagerank(networkx_graph)
+    expected_pagerank_results = np.empty(4)
+    for node, rank in networkx_pagerank.items():
+        expected_pagerank_results[node] = rank
+    # Load Graph Data
+    csv_data = "\n".join(
+        [f"{source},{destination}" for source, destination in networkx_graph_data]
+    )
+    csv_file = io.StringIO(csv_data)
     gdf = cudf.read_csv(
         csv_file, names=["Source", "Destination"], dtype=["int32", "int32"]
     )
     g = cugraph.DiGraph()
     g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-    # Validate Graph Data
-    assert g.number_of_vertices() == 3
-    assert g.number_of_edges() == 6
-    # Test PageRank
+    # Verify Graph Data
+    assert g.number_of_vertices() == 4
+    assert g.number_of_edges() == 5
+    # Verify Resolver Support For Graph Type
+    g_type = type(g)
+    assert g_type == cugraph.DiGraph
+    assert g_type in r.class_to_concrete
+    g_concrete_type = r.class_to_concrete[g_type]
+    assert g_concrete_type in r.concrete_types
+    # Verify Algorithm Presence
+    assert r.find_algorithm_exact("link_analysis.pagerank", g)
+    # Verify PageRank Result Type & Result
     rankings = r.algo.link_analysis.pagerank(g)
-    assert np.std(rankings.value) < 1e-8
-
-
-def test_pagerank_on_cugraph_digraph_via_fully_connected_graphs():
-    r = mg.resolver
-    # Generate & Load Graph Data
-    for number_of_nodes in [10, 100]:
-        sources = []
-        destinations = []
-        for node_a in range(number_of_nodes):
-            for node_b in range(node_a):
-                sources.append(node_a)
-                destinations.append(node_b)
-                sources.append(node_b)
-                destinations.append(node_a)
-        g = cugraph.DiGraph()
-        gdf = cudf.DataFrame({"Source": sources, "Destination": destinations})
-        g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-        # Validate Graph Data
-        assert g.number_of_vertices() == number_of_nodes
-        assert g.number_of_edges() == number_of_nodes * (number_of_nodes - 1)
-        # Test PageRank
-        rankings = r.algo.link_analysis.pagerank(g)
-        assert np.std(rankings.value) < 1e-8
-
-
-def test_pagerank_on_cugraph_digraph_via_star_like_graphs():
-    """
-    Generated graphs have a hub that all other nodes point to. 
-    A fraction of the outer nodes point to a neighbor that is not the hub.
-    """
-    r = mg.resolver
-    hub_node = 0
-    fraction_of_nodes_targetting_neighbor = 0.5
-    for number_of_nodes in [10, 100]:
-        # Generate & Load Graph Data
-        sources = []
-        destinations = []
-        all_nodes = range(number_of_nodes)
-        outer_nodes = filter(lambda node: node != hub_node, all_nodes)
-        expected_number_of_edges = 0
-        for outer_node in outer_nodes:
-            sources.append(outer_node)
-            destinations.append(hub_node)
-            expected_number_of_edges += 1
-            if outer_node / number_of_nodes > fraction_of_nodes_targetting_neighbor:
-                neighbor_node = outer_node - 1 if outer_node + 1 else outer_node + 1
-                sources.append(outer_node)
-                destinations.append(neighbor_node)
-                expected_number_of_edges += 1
-        g = cugraph.DiGraph()
-        gdf = cudf.DataFrame({"Source": sources, "Destination": destinations})
-        g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-        # Validate Graph Data
-        assert g.number_of_vertices() == number_of_nodes
-        assert g.number_of_edges() == expected_number_of_edges
-        # Test PageRank
-        rankings = r.algo.link_analysis.pagerank(g)
-        assert hub_node == 0
-        assert (rankings.value[hub_node] > rankings.value[hub_node + 1 :]).all()
+    assert isinstance(rankings.value, np.ndarray)
+    assert np.std(rankings.value - expected_pagerank_results) < 1e-4
