a5fd2a506d063f414b5290945a4e405a553d7c9b
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-03-26 15:07:30 -0700

    Update triangle count tests
    This commit removes all of the old triangle count tests, i.e.
    * test_triangle_count_on_cugraph_digraph_via_trivial_triangle_graph
    * test_triangle_count_on_cugraph_digraph_via_fully_connected_graph
    * test_triangle_count_on_cugraph_digraph_via_wheel_graph
    
    Those tests tested the backend implementations of those algorithms rather than metagraph-cuda itself. Testing those is out of scope since those implementations are managed in other projects outside of metagraph-cuda.
    
    We instead added the test test_triangle_count_on_cugraph_digraph that tests functionality that is within the scope of metagraph-cuda.
    
    This test does the following:
    * Verify that cugraph.DiGraph is supported directly in the resolver.
    * Verifies that the triangle count algorithm is present.
    * Verify that triangle count returns the desired output type (i.e. int).

--- metagraph_cuda/tests/algorithms/test_triangle_count.py
+++ metagraph_cuda/tests/algorithms/test_triangle_count.py
@@ -2,84 +2,58 @@ import metagraph as mg
 import cugraph
 import cudf
 import io
-import numpy as np
 
+# Simple graph with 5 triangles
+# 0 - 1    5 - 6
+# | X |    | /
+# 3 - 4 -- 2 - 7
 
-def test_triangle_count_on_cugraph_digraph_via_trivial_triangle_graph():
-    r = mg.resolver
-    # Load Graph Data
-    data = """
+graph_csv_data = """
 0,1
 1,0
-
-1,2
-2,1
-
-2,0
-0,2
+0,3
+3,0
+0,4
+4,0
+1,3
+3,1
+1,4
+4,1
+2,4
+4,2
+2,5
+5,2
+2,6
+6,2
+2,7
+7,2
+3,4
+4,3
+5,6
+6,5
 """
+
+
+def test_triangle_count_on_cugraph_digraph():
+    r = mg.resolver
+    # Load Graph Data
     csv_file = io.StringIO(data)
     gdf = cudf.read_csv(
         csv_file, names=["Source", "Destination"], dtype=["int32", "int32"]
     )
     g = cugraph.DiGraph()
     g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-    # Validate Graph Data
-    assert g.number_of_vertices() == 3
-    assert g.number_of_edges() == 6
-    # Test Triangle Count
-    assert r.algo.cluster.triangle_count(g) == 1
-
-
-def test_triangle_count_on_cugraph_digraph_via_fully_connected_graph():
-    r = mg.resolver
-    # Generate & Load Graph Data
-    number_of_nodes = 30
-    sources = []
-    destinations = []
-    for node_a in range(number_of_nodes):
-        for node_b in range(node_a):
-            sources.append(node_a)
-            destinations.append(node_b)
-            sources.append(node_b)
-            destinations.append(node_a)
-    g = cugraph.DiGraph()
-    gdf = cudf.DataFrame({"Source": sources, "Destination": destinations})
-    g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-    # Validate Graph Data
-    assert g.number_of_vertices() == number_of_nodes
-    # n * (n-1) / 2 * two_edges_per_direction
-    assert g.number_of_edges() == number_of_nodes * (number_of_nodes - 1)
-    # Test Triangle Count
-    assert r.algo.cluster.triangle_count(g) == 4060  # 4060 == 30 choose 3
-
-
-def test_triangle_count_on_cugraph_digraph_via_wheel_graph():
-    r = mg.resolver
-    # Generate & Load Graph Data
-    for number_of_nodes in [10, 100, 1_000, 10_000, 100_000]:
-        hub_node = 0
-        sources = []
-        destinations = []
-        all_nodes = range(number_of_nodes)
-        outer_nodes = filter(lambda node: node != hub_node, all_nodes)
-        for outer_node in outer_nodes:
-            neighbor_node = outer_node - 1
-            if neighbor_node == hub_node:
-                neighbor_node = (hub_node - 1) % number_of_nodes
-            sources.append(outer_node)
-            destinations.append(hub_node)
-            sources.append(hub_node)
-            destinations.append(outer_node)
-            sources.append(outer_node)
-            destinations.append(neighbor_node)
-            sources.append(neighbor_node)
-            destinations.append(outer_node)
-        g = cugraph.DiGraph()
-        gdf = cudf.DataFrame({"Source": sources, "Destination": destinations})
-        g.from_cudf_edgelist(gdf, source="Source", destination="Destination")
-        # Validate Graph Data
-        assert g.number_of_vertices() == number_of_nodes
-        assert g.number_of_edges() == (number_of_nodes - 1) * 4
-        # Test Triangle Count
-        assert r.algo.cluster.triangle_count(g) == (number_of_nodes - 1)
+    # Verify Graph Data
+    assert g.number_of_vertices() == 8
+    assert g.number_of_edges() == 22
+    # Verify Resolver Support For Graph Type
+    g_type = type(g)
+    assert g_type == cugraph.DiGraph
+    assert g_type in r.class_to_concrete
+    g_concrete_type = r.class_to_concrete[g_type]
+    assert g_concrete_type in r.concrete_types
+    # Verify Algorithm Presence
+    assert r.find_algorithm_exact("cluster.triangle_count", g)
+    # Verify Triangle Count Result Type
+    number_of_triangles = r.algo.cluster.triangle_count(g)
+    assert isinstance(number_of_triangles, int)
