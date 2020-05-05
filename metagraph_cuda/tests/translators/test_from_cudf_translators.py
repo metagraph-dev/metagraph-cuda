5b8b4ce9216406e6703b972ff4bc1ae0e6e3268a
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-05-05 13:01:17 -0700

    Make test_from_cudf_translators.py use assert_equals
    This commit makes the tests in test_from_cudf_translators.py use assert_equals to follow the convention established in the other test files.

--- metagraph_cuda/tests/translators/test_from_cudf_translators.py
+++ metagraph_cuda/tests/translators/test_from_cudf_translators.py
@@ -3,6 +3,8 @@ import pandas as pd
 import cugraph
 import cudf
 import io
+from metagraph.plugins.pandas.types import PandasEdgeList
+from metagraph.plugins.python.types import PythonNodes
 
 
 def test_cudf_edge_list_to_pandas_edge_list():
@@ -18,7 +20,7 @@ def test_cudf_edge_list_to_pandas_edge_list():
 |0|       |2|  <--  |3|
 +-+  -->  +-+       +-+
 """
-    r = mg.resolver
+    dpr = mg.resolver
     csv_data = """
 Source,Destination
 0,1
@@ -29,35 +31,27 @@ Source,Destination
 """
     csv_file = io.StringIO(csv_data)
     cdf_unwrapped = cudf.read_csv(csv_file)
-    cdf = r.wrapper.Graph.CuDFEdgeList(cdf_unwrapped, "Source", "Destination")
-    pdf = r.translate(cdf, r.types.Graph.PandasEdgeListType)
-    assert len(pdf.value) == len(pdf.value)
-    assert len(pdf.value) == 5
-    assert set(pdf.value[pdf.value["Source"] == 0]["Destination"]) == set(
-        pdf.value[pdf.value["Source"] == 0]["Destination"]
+    x = dpr.wrapper.Graph.CuDFEdgeList(cdf_unwrapped, "Source", "Destination")
+
+    sources = [0, 0, 1, 2, 3]
+    destinations = [1, 2, 2, 0, 2]
+    pdf = pd.DataFrame(
+        {"source": sources, "destination": destinations},
+        columns=["source", "destination"],
     )
-    assert set(pdf.value[pdf.value["Source"] == 0]["Destination"]) == {1, 2}
-    assert set(pdf.value[pdf.value["Source"] == 1]["Destination"]) == set(
-        pdf.value[pdf.value["Source"] == 1]["Destination"]
-    )
-    assert set(pdf.value[pdf.value["Source"] == 1]["Destination"]) == {2}
-    assert set(pdf.value[pdf.value["Source"] == 2]["Destination"]) == set(
-        pdf.value[pdf.value["Source"] == 2]["Destination"]
-    )
-    assert set(pdf.value[pdf.value["Source"] == 2]["Destination"]) == {0}
-    assert set(pdf.value[pdf.value["Source"] == 3]["Destination"]) == set(
-        pdf.value[pdf.value["Source"] == 3]["Destination"]
-    )
-    assert set(pdf.value[pdf.value["Source"] == 3]["Destination"]) == {2}
+    intermediate = PandasEdgeList(pdf, "source", "destination")
+    y = dpr.translate(x, PandasEdgeList)
+    PandasEdgeList.Type.assert_equal(y, intermediate)
 
 
 def test_cudf_nodes_to_python_nodes():
-    r = mg.resolver
+    dpr = mg.resolver
     keys = [3, 2, 1]
     values = [33, 22, 11]
     cudf_data = cudf.DataFrame({"key": keys, "val": values})
-    cudf_nodes = r.wrapper.Nodes.CuDFNodes(cudf_data, "key", "val")
-    python_nodes = r.translate(cudf_nodes, r.types.Nodes.PythonNodesType)
-    assert len(cudf_nodes.value) == 3
-    for k, v in zip(keys, values):
-        assert python_nodes[k] == v
+    x = dpr.wrapper.Nodes.CuDFNodes(cudf_data, "key", "val")
+
+    python_dict = {k: v for k, v in zip(keys, values)}
+    intermediate = PythonNodes(python_dict)
+    y = dpr.translate(x, PythonNodes)
+    PythonNodes.Type.assert_equal(y, intermediate)
